77ff27e2e3335041099a23e0a6be64f8e75b6770
Mike Reinstein <mreinstein@sellpoints.com>
2020-06-30 01:06:01 -0700

    wire up skeleton property events

--- property.js
+++ property.js
@@ -11,14 +11,52 @@ function init (options={}) {
 
 
 function view (model, update) {
+
     return html`<div class="property-set-contents">
         ${model.property.properties.map((p) => {
+
+            const _blurLabel = function (ev) {
+                
+                // TODO
+            }
+
+            const _keydownLabel = function (ev) {
+                // TODO
+            }
+
+            const _focusLabel = function (ev) {
+
+                // TODO
+            }
+
+            const _blurVal = function (ev) {
+                // TODO
+            }
+
+            const _keydownVal = function (ev) {
+                // TODO
+            }
+
+            const _focusVal = function (ev) {
+                // TODO
+            }
+
             return html`<div class="property-field">
                 <div class="property-label">
-                    <input type="text" value="${p.label}">
+                    <input type="text"
+                           value="${p.label}"
+                           @on:blur=${_blurLabel}
+                           @on:focus=${_focusLabel}
+                           @on:keydown=${_keydownLabel}>
                 </div>
 
-                <div class="c"><input type="text" value="${p.value}"></div>
+                <div class="c">
+                    <input type="text"
+                           value="${p.value}"
+                           @on:blur=${_blurVal}
+                           @on:focus=${_focusVal}
+                           @on:keydown=${_keydownVal}>
+                </div>
             </div>`
         })}
     </div>`
