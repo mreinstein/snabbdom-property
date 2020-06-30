f4e0b68d330e4284a2d7bc746aff031ebb4749f2
Mike Reinstein <mreinstein@sellpoints.com>
2020-06-30 00:56:57 -0700

    wire up more of the property panel

--- property.js
+++ property.js
@@ -3,91 +3,24 @@ import html from 'https://cdn.jsdelivr.net/npm/snabby@1/snabby.js'
 
 function init (options={}) {
     const model = {
-        properties: [
-            {
-                name: 'name',
-                type: 'string',
-                value: 'root'
-            },
-            {
-                name: 'display',
-                type: 'string',
-                value: 'grid'
-            },
-            {
-                name: 'grid-template-columns',
-                type: 'string',
-                value: 'repeat(2, 1fr)'
-            },
-            {
-                name: 'grid-template-rows',
-                type: 'string',
-                value: 'repeat(3, 1fr)'
-            },
-            {
-                name: 'grid-gap',
-                type: 'string',
-                value: '8px'
-            },
-            {
-                name: 'height',
-                type: 'string',
-                value: '100%'
-            }               
-        ]
+        properties: [ ...options.properties ]
     }
+
     return model
 }
 
 
 function view (model, update) {
     return html`<div class="property-set-contents">
-        <div class="property-field">
-            <div class="property-label">
-                <input type="text" value="name">
-            </div>
+        ${model.property.properties.map((p) => {
+            return html`<div class="property-field">
+                <div class="property-label">
+                    <input type="text" value="${p.label}">
+                </div>
 
-            <div class="c"><input type="text" value="root"></div>
-        </div>
-
-        <div class="property-field">
-            <div class="property-label">
-                <input type="text" value="display">
-            </div>
-            <div class="c"><input type="text" value="grid"></div>
-
-        </div>
-
-        <div class="property-field">
-            <div class="property-label">
-                <input type="text" value="grid-template-columns">
-            </div>
-            <div class="c">
-                <input type="text" value="repeat(2, 1fr)">
-            </div>
-
-        </div>
-
-        <div class="property-field">
-            <div class="property-label">
-                <input type="text" value="grid-template-rows">
-            </div>
-            <div class="c"><input type="text" value="repeat(3, 1fr)"></div>
-        </div>
-
-        <div class="property-field">
-            <div class="property-label">
-                <input type="text" value="grid-gap">
-            </div>
-            <div class="c"><input type="text" value="8px"></div>
-        </div>
-
-        <div class="property-field">
-            <div class="property-label">
-                <input type="text" value="height">
-            </div>
-            <div class="c"><input type="text" value="100%"></div>
-        </div>
+                <div class="c"><input type="text" value="${p.value}"></div>
+            </div>`
+        })}
     </div>`
 }
 
